c9adf8d67c952cf14bdfeb6af9eb00017f0035df
Jason <jrmoynihan99@gmail.com>
2026-01-19 20:57:54 -0500

    added syncing build number script

---
+++ ios/scripts/sync-app-clip-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get the main app's build number
+MAIN_BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/Anchor/Info.plist")
+
+# Set the App Clip's build number to match
+/usr/libexec/PlistBuddy -c "Set CFBundleVersion $MAIN_BUILD_NUMBER" "${PROJECT_DIR}/Clip/Info.plist"
+
+echo "Synced App Clip build number to: $MAIN_BUILD_NUMBER"
