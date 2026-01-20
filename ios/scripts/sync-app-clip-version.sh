a3d5f2a92463f5883efd8385e635c05f53328ec7
Jason <jrmoynihan99@gmail.com>
2026-01-19 21:16:23 -0500

    fixed path in sync number script

--- ios/scripts/sync-app-clip-version.sh
+++ ios/scripts/sync-app-clip-version.sh
@@ -4,6 +4,6 @@
 MAIN_BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/Anchor/Info.plist")
 
 # Set the App Clip's build number to match
-/usr/libexec/PlistBuddy -c "Set CFBundleVersion $MAIN_BUILD_NUMBER" "${PROJECT_DIR}/Clip/Info.plist"
+/usr/libexec/PlistBuddy -c "Set CFBundleVersion $MAIN_BUILD_NUMBER" "${PROJECT_DIR}/AnchorClip/Info.plist"
 
 echo "Synced App Clip build number to: $MAIN_BUILD_NUMBER"
