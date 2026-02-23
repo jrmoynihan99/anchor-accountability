da67f12516f018e9db17e4c5c9d367b1bbeb3080
Jason Moynihan <jasonmoynihan@Jasons-MacBook-Air.local>
2026-02-23 15:12:08 -0500

    Initial Widget build succeeded

--- ios/AnchorWidget/AnchorWidgetBundle.swift
+++ ios/AnchorWidget/AnchorWidgetBundle.swift
@@ -1,10 +1,17 @@
+//
+//  AnchorWidgetBundle.swift
+//  AnchorWidget
+//
+//  Created by Jason Moynihan on 2/23/26.
+//
+
 import WidgetKit
 import SwiftUI
 
 @main
 struct AnchorWidgetBundle: WidgetBundle {
     var body: some Widget {
-        QRCodeWidget()
-        ReachOutWidget()
+      QRCodeWidget()
+      ReachOutWidget()
     }
 }
--- ios/AnchorWidget/ReachOutWidgetView.swift
+++ ios/AnchorWidget/ReachOutWidgetView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import WidgetKit
+import AppIntents
 
 struct ReachOutWidgetView: View {
     let entry: ReachOutEntry
