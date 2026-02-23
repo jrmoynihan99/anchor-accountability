9531d49c40fe326103e0906890a9e572edaba8e3
Jason <jrmoynihan99@gmail.com>
2026-02-23 15:42:18 -0500

    made changes to the app group files are root in case

--- ios/AppGroupStorage.swift
+++ ios/AppGroupStorage.swift
@@ -7,12 +7,20 @@
 
 import Foundation
 import React
+import WidgetKit
 
 @objc(AppGroupStorage)
 class AppGroupStorage: NSObject {
 
   private let suiteName = "group.com.jrmoynihan99.anchor"
   private let orgKey = "pendingOrganization"
+
+  // Widget data keys
+  private let widgetOrgIdKey = "widget_orgId"
+  private let widgetOrgNameKey = "widget_orgName"
+  private let widgetDeepLinkKey = "widget_deepLink"
+  private let widgetUserIdKey = "widget_userId"
+  private let widgetTokenKey = "widget_token"
 
   @objc
   func getDeferredOrg(
@@ -26,7 +34,7 @@ class AppGroupStorage: NSObject {
     if org != nil {
       defaults?.removeObject(forKey: orgKey)
     }
-    
+
     resolve(org)
   }
 
@@ -38,6 +46,55 @@ class AppGroupStorage: NSObject {
   ) {
     let defaults = UserDefaults(suiteName: suiteName)
     defaults?.set(org, forKey: orgKey)
+    resolve(true)
+  }
+
+  @objc
+  func setWidgetData(
+    _ data: NSDictionary,
+    resolver resolve: RCTPromiseResolveBlock,
+    rejecter reject: RCTPromiseRejectBlock
+  ) {
+    let defaults = UserDefaults(suiteName: suiteName)
+    if let orgId = data["orgId"] as? String {
+      defaults?.set(orgId, forKey: widgetOrgIdKey)
+    }
+    if let orgName = data["orgName"] as? String {
+      defaults?.set(orgName, forKey: widgetOrgNameKey)
+    }
+    if let deepLink = data["deepLink"] as? String {
+      defaults?.set(deepLink, forKey: widgetDeepLinkKey)
+    }
+    if let userId = data["userId"] as? String {
+      defaults?.set(userId, forKey: widgetUserIdKey)
+    }
+    if let widgetToken = data["widgetToken"] as? String {
+      defaults?.set(widgetToken, forKey: widgetTokenKey)
+    }
+    resolve(true)
+  }
+
+  @objc
+  func clearWidgetData(
+    _ resolve: RCTPromiseResolveBlock,
+    rejecter reject: RCTPromiseRejectBlock
+  ) {
+    let defaults = UserDefaults(suiteName: suiteName)
+    [widgetOrgIdKey, widgetOrgNameKey, widgetDeepLinkKey,
+     widgetUserIdKey, widgetTokenKey].forEach {
+      defaults?.removeObject(forKey: $0)
+    }
+    resolve(true)
+  }
+
+  @objc
+  func reloadWidgetTimelines(
+    _ resolve: RCTPromiseResolveBlock,
+    rejecter reject: RCTPromiseRejectBlock
+  ) {
+    if #available(iOS 14.0, *) {
+      WidgetCenter.shared.reloadAllTimelines()
+    }
     resolve(true)
   }
 
